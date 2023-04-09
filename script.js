a4dfe9ab46b3eebbac4f562302484ca13c4fe70c
Kaushal <kaushalkundu5683@gmail.com>
2023-04-09 20:04:34 +0530

    Added variable for input values

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
-add = (a, b) => {
-    return a + b;
+const calculation = () => {
+    let num1 = Number(prompt("Enter 1st number"));
+    let operator = prompt("Enter operator");
+    let num2 = Number(prompt("Enter 2nd number"));
+
+    if (operator == "+") {
+        return num1 + num2;
+    }
+    else if (operator == "-") {
+        return num1 - num2;
+    }
+    else if (operator == "*") {
+        return num1 * num2;
+    }
+    else if (operator == "/") {
+        return num1 / num2;
+    }
+    else {
+        return "Please only enter: +, -, * or /";
+    }
 };
 
-subtract = (a, b) => {
-    return a - b;
-};
-
-multiply = (a, b) => {
-    return a * b;
-};
-
-divide = (a, b) => {
-    return a / b;
-};
+console.log(calculation());
